c71eb0d62d12a479fb41711f9a331efc3283de2a
Matthew Yang <yang70@gmail.com>
2018-02-21 13:54:54 -0800

    CLIENT-1739 remove unused resources folder and commented code

--- unimatrix_sdk/resource.go
+++ unimatrix_sdk/resource.go
@@ -1,26 +1,5 @@
 package unimatrix_sdk
 
-// var typeRegistry = make(map[string]reflect.Type)
-//
-// func registerType(myType interface{}) {
-// 	t := reflect.TypeOf(myType).Elem()
-// 	typeRegistry[t.Name()] = t
-// }
-//
-// type Artifact struct {
-// 	name string
-// }
-//
-// func init() {
-// 	registerType((*Artifact)(nil))
-// }
-//
-// func makeInstance(name string) interface{} {
-// 	v := reflect.New(typeRegistry[name]).Elem()
-// 	// Maybe fill in fields here if necessary
-// 	return v.Interface()
-// }
-
 type Resource struct {
 	attributes map[string]interface{}
 }
