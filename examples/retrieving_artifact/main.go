1c6c97199ce076a7a31913037649f7aef588d6f6
Matthew Yang <yang70@gmail.com>
2018-02-21 20:34:18 -0800

    CLIENT-1694 get api base url from config

--- examples/retrieving_artifact/main.go
+++ examples/retrieving_artifact/main.go
@@ -7,8 +7,8 @@ import (
 )
 
 func main() {
-	apiUrl := "http://archivist-acceptance-1784742539.us-west-2.elb.amazonaws.com"
-	operation := unimatrix.NewOperation(apiUrl + "/realms/1e338862026376dd593425404a4f75c0/artifacts")
+	unimatrix.SetURL("http://archivist-acceptance-1784742539.us-west-2.elb.amazonaws.com")
+	operation := unimatrix.NewOperation("/realms/1e338862026376dd593425404a4f75c0/artifacts")
 	params := map[string]string{"uuid": "643a9e056300d54eed7a14066513f435"}
 	operation.SetParameters(params)
 	response, _ := operation.Read()
