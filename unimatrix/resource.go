68ea9b2f98c81958b49ddda43a6ccde3b0073383
Matthew Yang <yang70@gmail.com>
2018-02-21 17:28:24 -0800

    expand argument variable name in resource.go

--- unimatrix/resource.go
+++ unimatrix/resource.go
@@ -4,6 +4,6 @@ type Resource struct {
 	attributes map[string]interface{}
 }
 
-func NewResource(name string, r interface{}) *Resource {
-	return &Resource{attributes: r.(map[string]interface{})}
+func NewResource(name string, resource interface{}) *Resource {
+	return &Resource{attributes: resource.(map[string]interface{})}
 }
