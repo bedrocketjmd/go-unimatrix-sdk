e15705a967ab1055f23e2f57d15da2421a72af86
Chris Roggi <croggi18@gmail.com>
2018-02-21 21:47:56 -0500

    CLIENT-1745 Simplify resource type to map[string]string

---
+++ examples/retrieving_artifact_associations/main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+
+	"../../unimatrix"
+)
+
+func main() {
+	apiUrl := "http://archivist-acceptance-1784742539.us-west-2.elb.amazonaws.com"
+	operation := unimatrix.NewOperation(apiUrl + "/realms/1e338862026376dd593425404a4f75c0/artifacts/4053b259918f6a81b0cac7f2a0e78dcc?include[relationships.category][artifacts]=true")
+	response, _ := operation.Read()
+
+	for _, resource := range response.Resources {
+		fmt.Println(response.GetAssociations("artifacts", resource["id"]))
+	}
+}
