d5b8daf696b7bd18fe4619df8eb994321b12e105
Alena Wang <alena.n.wang@gmail.com>
2018-02-23 16:49:42 -0800

    CLIENT-1741 Add additional query methods count, offset, sort

--- unimatrix/query.go
+++ unimatrix/query.go
@@ -1,34 +1,45 @@
 package unimatrix
 
 import (
-	"net/url"
+	"strconv"
 )
 
 type Query struct {
-	parameters url.Values
+	parameters map[string][]string
 }
 
 func NewQuery() *Query {
-	parameters := url.Values{}
-	return &Query{
-		parameters: parameters,
-	}
+	return &Query{parameters: map[string][]string{}}
 }
 
-func (query *Query) Parameters() string {
-	return query.parameters.Encode()
+func (query *Query) Parameters() map[string][]string {
+	return query.parameters
 }
 
 func (query *Query) Where(parameter, value string) *Query {
-	query.parameters.Add(parameter, value)
+	query.parameters[parameter] = []string{value}
 	return query
 }
 
 func (query *Query) WhereArray(parameter string, values []string) *Query {
 	parameter += "[]"
-	for _, value := range values {
-		query.parameters.Add(parameter, value)
-	}
+	query.parameters[parameter] = values
+	return query
+}
+
+func (query *Query) Count(value int) *Query {
+	query.parameters["count"] = []string{strconv.Itoa(value)}
+	return query
+}
+
+func (query *Query) Offset(value int) *Query {
+	query.parameters["offset"] = []string{strconv.Itoa(value)}
+	return query
+}
+
+func (query *Query) Sort(sort_by, sort_direction string) *Query {
+	query.parameters["sort_by"] = []string{sort_by}
+	query.parameters["sort_direction"] = []string{sort_direction}
 	return query
 }
 
@@ -37,6 +48,6 @@ func (query *Query) Include(parameters ...string) *Query {
 	for _, parameter := range parameters {
 		includeParameter += "[" + parameter + "]"
 	}
-	query.parameters.Add(includeParameter, "true")
+	query.parameters[includeParameter] = []string{"true"}
 	return query
 }
