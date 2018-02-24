e5c7c480bbc1c1fbb9f5051e07a3115e93d57e42
Alena Wang <alena.n.wang@gmail.com>
2018-02-23 16:52:03 -0800

    CLIENT-1741 Update querying for artifact example with additional method calls

--- examples/querying_for_artifacts/main.go
+++ examples/querying_for_artifacts/main.go
@@ -9,16 +9,38 @@ import (
 func main() {
 	unimatrix.SetURL("http://us-west-2.api.acceptance.unimatrix.io")
 	operation := unimatrix.NewOperation("/realms/5cbc6bb3db90e2f1236e005f9054776c/artifacts")
+
+	// assign query parameters directly with a map[string][]string
+	parameters := map[string][]string{"test": []string{"test_value"}}
+	operation.AssignParameters(parameters)
+
+	// create a new query
 	query := unimatrix.NewQuery().
 		Where("type_name:eq", "video_artifact").
 		WhereArray("name:in", []string{"Final Nick", "test with categoryyy"}).
 		WhereArray("relationships.category:eq", []string{"59419fff92047f6e01a98ed35eb21f10"}).
 		Where("description:search", "stuff")
+	fmt.Println("QUERY PARAMETERS:")
 	fmt.Println(query.Parameters())
 
-	operation.SetParameters(query.Parameters())
-	response, _ := operation.Read()
+	// assigning query parameters will replace previous query parameters
+	operation.AssignParameters(query.Parameters())
 
+	additional_query := unimatrix.NewQuery().
+		Count(5).
+		Offset(3).
+		Sort("updated_at", "desc").
+		WhereArray("name:in", []string{"Another Field"})
+	fmt.Println("\nADDITIONAL QUERY PARAMETERS:")
+	fmt.Println(query.Parameters())
+
+	// appending query parameters will add to previous query parameters
+	operation.AppendParameters(additional_query.Parameters())
+	fmt.Println("\nOPERATION:")
+	fmt.Println(operation)
+
+	response, _ := operation.Read()
+	fmt.Println("\nRESPONSE:")
 	for _, resource := range response.Resources {
 		fmt.Println(resource)
 	}
