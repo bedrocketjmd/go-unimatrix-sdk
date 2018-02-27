dcb519fe632955a62cb42337099355262d2224f0
Alena Wang <alena.n.wang@gmail.com>
2018-02-27 15:08:12 -0800

    CLIENT-1780 Update examples to use RealmScopedOperation

--- examples/retrieving_artifact_associations/main.go
+++ examples/retrieving_artifact_associations/main.go
@@ -8,7 +8,10 @@ import (
 
 func main() {
 	unimatrix.SetURL("http://us-west-2.api.acceptance.unimatrix.io")
-	operation := unimatrix.NewOperation("/realms/1e338862026376dd593425404a4f75c0/artifacts")
+	operation := unimatrix.NewRealmScopedOperation(
+		"1e338862026376dd593425404a4f75c0",
+		"artifacts",
+	)
 
 	query := unimatrix.NewQuery().
 		Where("uuid", "4053b259918f6a81b0cac7f2a0e78dcc").
